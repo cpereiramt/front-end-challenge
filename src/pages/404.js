541bd8379892834447d97190882e360cb507313b
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-09-15 11:34:46 -0400

    Setting Gatsby on project

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Page404 = () =>  {
+  return (
+     <Layout>
+     <h1> Error page not found ! </h1> 
+     </Layout>
+  )
+}
+
+export default Page404;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const index = () =>  {
+  return (
+      <Layout>
+     <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+     </Layout>
+    )
+}
+
+export default index;
